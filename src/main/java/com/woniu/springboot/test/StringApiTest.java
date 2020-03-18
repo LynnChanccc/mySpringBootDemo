491f535d5ff0af04bd97f0973e26f53f8f6fa8fc
LynnChanccc <919990358@qq.com>
2020-03-18 14:43:27 +0800

    加密算法demo

--- src/main/java/com/woniu/springboot/test/StringApiTest.java
+++ src/main/java/com/woniu/springboot/test/StringApiTest.java
@@ -1,9 +1,12 @@
 package com.woniu.springboot.test;
 
+import java.util.ArrayList;
+import java.util.Queue;
+
 /**
  * @author cl
  * @Date 2020/3/10 13:08
- * 字符串API测试
+ * 字符串API测试 加密
  */
 public class StringApiTest {
     public static void main(String[] args) {
@@ -13,5 +16,7 @@ public class StringApiTest {
         //删除最后一个","号，动态拼接有时可能会用上
         sb.deleteCharAt(sb.lastIndexOf(","));
         System.out.println(sb);
+
+
     }
 }
---
+++ src/main/java/com/woniu/springboot/test/encryptTest/EncryptDemo.java
@@ -0,0 +1,102 @@
+package com.woniu.springboot.test.encryptTest;
+
+import org.junit.Test;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+/**
+ * @author cl
+ * @Date 2020/3/18 14:01
+ * 加密算法
+ */
+public class EncryptDemo {
+
+    //定义8字节长度的秘钥
+    private static String keyTextDES = "34er2323";
+    //定义8字节长度的偏移量
+    private static String ivStrDES = "12bv4bvk";
+    private static byte[] ivDES = ivStrDES.getBytes();
+
+    //定义16字节长度秘钥
+    private static String keyTextAES = "34er232312bv4bvk";
+
+    //DES加密
+    public static String encryptDES(String string) throws Exception{
+        //生成向量
+        IvParameterSpec zeroIv = new IvParameterSpec(ivDES);
+        //根据秘钥字符串生成秘钥对象
+        SecretKeySpec key = new SecretKeySpec(keyTextDES.getBytes(), "DES");
+        //实例化密码类 用于加密和解密 实例化时需传入加解密方式
+        Cipher cipher = Cipher.getInstance("DES/CBC/PKCS5Padding");
+        //初始化密码类 设置秘钥 向量 选择模式（加密还是解密）
+        cipher.init(Cipher.ENCRYPT_MODE,key,zeroIv);
+        //加密得到密文
+        byte[] encryptData = cipher.doFinal(string.getBytes());
+        //使用Base64工具类对密文字节数据进行编码
+        return Base64.getEncoder().encodeToString(encryptData);
+    }
+
+    //DES解密
+    public static String decryptDES(String str) throws Exception{
+        //使用Base64工具类对密文进行解码
+        byte[] decode = Base64.getDecoder().decode(str);
+        //实例化向量
+        IvParameterSpec zeroIv = new IvParameterSpec(ivDES);
+        //生成秘钥
+        SecretKeySpec key = new SecretKeySpec(keyTextDES.getBytes(), "DES");
+        //实例化密码类
+        Cipher cipher = Cipher.getInstance("DES/CBC/PKCS5Padding");
+        //初始化密码类 设置秘钥 向量 选择解密方式
+        cipher.init(Cipher.DECRYPT_MODE,key,zeroIv);
+        //使用密码类对密文进行解密
+        byte[] decryptData = cipher.doFinal(decode);
+        return new String(decryptData);
+    }
+
+    //AES加密
+    public static String encryptAES(String encryptStr) throws Exception{
+        //根据秘钥字符串生成秘钥对象
+        SecretKeySpec key = new SecretKeySpec(keyTextAES.getBytes(), "AES");
+        //实例化密码类 用于加密和解密 实例化时需传入加解密方式
+        Cipher cipher = Cipher.getInstance("AES");
+        //初始化密码类 设置秘钥 向量 选择加密方式
+        cipher.init(Cipher.ENCRYPT_MODE,key);
+        //加密得到密文
+        byte[] encryptData = cipher.doFinal(encryptStr.getBytes());
+        return Base64.getEncoder().encodeToString(encryptData);
+    }
+
+    //AES解密
+    public static String decryptAES(String decryptStr)throws Exception{
+        byte[] decode = Base64.getDecoder().decode(decryptStr);
+        SecretKeySpec key = new SecretKeySpec(keyTextAES.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE,key);
+        byte[] decryptData = cipher.doFinal(decode);
+        return new String(decryptData);
+    }
+
+    @Test
+    public void test() throws Exception{
+        //DES加密
+        String encryptDES = EncryptDemo.encryptDES("123456");
+        System.out.println(encryptDES);
+        //DES解密
+        String decryptDES = EncryptDemo.decryptDES(encryptDES);
+        System.out.println(decryptDES);
+
+        //AES加密
+        String encryptAES = EncryptDemo.encryptAES("123456");
+        System.out.println(encryptAES);
+        //AES解密
+        String decryptAES = EncryptDemo.decryptAES(encryptAES);
+        System.out.println(decryptAES);
+
+    }
+
+
+}
