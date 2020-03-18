fb0dafc5d9ee6170c515e7e0468f7ce55085d699
LynnChanccc <919990358@qq.com>
2020-03-18 14:53:57 +0800

    新增摘要加密demo

--- src/main/java/com/woniu/springboot/test/encryptTest/EncryptDemo.java
+++ src/main/java/com/woniu/springboot/test/encryptTest/EncryptDemo.java
@@ -3,9 +3,9 @@ package com.woniu.springboot.test.encryptTest;
 import org.junit.Test;
 
 import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.security.MessageDigest;
 import java.util.Base64;
 
 /**
@@ -25,7 +25,7 @@ public class EncryptDemo {
     private static String keyTextAES = "34er232312bv4bvk";
 
     //DES加密
-    public static String encryptDES(String string) throws Exception{
+    public static String encryptDES(String string) throws Exception {
         //生成向量
         IvParameterSpec zeroIv = new IvParameterSpec(ivDES);
         //根据秘钥字符串生成秘钥对象
@@ -33,7 +33,7 @@ public class EncryptDemo {
         //实例化密码类 用于加密和解密 实例化时需传入加解密方式
         Cipher cipher = Cipher.getInstance("DES/CBC/PKCS5Padding");
         //初始化密码类 设置秘钥 向量 选择模式（加密还是解密）
-        cipher.init(Cipher.ENCRYPT_MODE,key,zeroIv);
+        cipher.init(Cipher.ENCRYPT_MODE, key, zeroIv);
         //加密得到密文
         byte[] encryptData = cipher.doFinal(string.getBytes());
         //使用Base64工具类对密文字节数据进行编码
@@ -41,7 +41,7 @@ public class EncryptDemo {
     }
 
     //DES解密
-    public static String decryptDES(String str) throws Exception{
+    public static String decryptDES(String str) throws Exception {
         //使用Base64工具类对密文进行解码
         byte[] decode = Base64.getDecoder().decode(str);
         //实例化向量
@@ -51,37 +51,61 @@ public class EncryptDemo {
         //实例化密码类
         Cipher cipher = Cipher.getInstance("DES/CBC/PKCS5Padding");
         //初始化密码类 设置秘钥 向量 选择解密方式
-        cipher.init(Cipher.DECRYPT_MODE,key,zeroIv);
+        cipher.init(Cipher.DECRYPT_MODE, key, zeroIv);
         //使用密码类对密文进行解密
         byte[] decryptData = cipher.doFinal(decode);
         return new String(decryptData);
     }
 
     //AES加密
-    public static String encryptAES(String encryptStr) throws Exception{
+    public static String encryptAES(String encryptStr) throws Exception {
         //根据秘钥字符串生成秘钥对象
         SecretKeySpec key = new SecretKeySpec(keyTextAES.getBytes(), "AES");
         //实例化密码类 用于加密和解密 实例化时需传入加解密方式
         Cipher cipher = Cipher.getInstance("AES");
         //初始化密码类 设置秘钥 向量 选择加密方式
-        cipher.init(Cipher.ENCRYPT_MODE,key);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
         //加密得到密文
         byte[] encryptData = cipher.doFinal(encryptStr.getBytes());
         return Base64.getEncoder().encodeToString(encryptData);
     }
 
     //AES解密
-    public static String decryptAES(String decryptStr)throws Exception{
+    public static String decryptAES(String decryptStr) throws Exception {
         byte[] decode = Base64.getDecoder().decode(decryptStr);
         SecretKeySpec key = new SecretKeySpec(keyTextAES.getBytes(), "AES");
         Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(Cipher.DECRYPT_MODE,key);
+        cipher.init(Cipher.DECRYPT_MODE, key);
         byte[] decryptData = cipher.doFinal(decode);
         return new String(decryptData);
     }
 
+    //摘要算法加密 MD5 不可逆
+    public static String encryptMD5(String encryptStr) throws Exception {
+
+        //根据MD5算法生成MessageDigest对象
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        byte[] srcBytes = encryptStr.getBytes();
+        //使用srcBytes更新摘要
+        md5.update(srcBytes);
+        //完成哈希计算 得到结果
+        byte[] resultData = md5.digest();
+        return Base64.getEncoder().encodeToString(resultData);
+    }
+
+    //摘要算法加密 SHA 不可逆
+    public static String encryptSHA(String encryptStr) throws Exception {
+        //使用SHA算法生成MessageDigest对象
+        MessageDigest sha = MessageDigest.getInstance("SHA");
+        byte[] bytes = encryptStr.getBytes();
+        sha.update(bytes);
+        byte[] resultData = sha.digest();
+        return Base64.getEncoder().encodeToString(resultData);
+    }
+
+
     @Test
-    public void test() throws Exception{
+    public void test() throws Exception {
         //DES加密
         String encryptDES = EncryptDemo.encryptDES("123456");
         System.out.println(encryptDES);
@@ -95,6 +119,14 @@ public class EncryptDemo {
         //AES解密
         String decryptAES = EncryptDemo.decryptAES(encryptAES);
         System.out.println(decryptAES);
+
+        //MD5加密
+        String encryptMD5 = EncryptDemo.encryptMD5("123456");
+        //SHA加密
+        String encryptSHA = EncryptDemo.encryptSHA("123456");
+        System.out.println(encryptMD5);
+        System.out.println(encryptSHA);
+
 
     }
 
